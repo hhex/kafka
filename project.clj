9dd16b1ddf4311a6a1600cc0a9ad97bfbd770039
Heinz R. Hopfgartner <Heinz@178.115.73.40.wireless.dyn.drei.com>
2010-10-17 09:00:55 +0200

    Cleaned up for publishing on clojars.org

--- project.clj
+++ project.clj
@@ -5,14 +5,9 @@
                  ]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      ]
-  ;; [mycroft/mycroft "0.0.2"]
-  ;; These namespaces will be AOT-compiled. Needed for gen-class and
-  ;; other Java interop functionality. :namespaces is an alias for this.
   :aot [kafka.applet]
-  ;; Leave the contents of :source-path out of jars (for AOT projects)
-  ;;:omit-source true
-  ;; Emit warnings on all reflection calls.
+
   :warn-on-reflection true
-  :jar-name "kafka.jar" ;; applets mag den default jar Namen nicht
+  :jar-name "kafka.jar"
   :uberjar-name "kafka-full.jar"
   )
